8eae6b0c46efcd82acb202c963c738ef10a3cfab
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-01 10:57:45 +0100

    Update DiscordClient.js

--- src/Classes/DiscordClient.js
+++ src/Classes/DiscordClient.js
@@ -5,14 +5,20 @@ import Gateway from "./Gateway"
 class DiscordClient {
     constructor(Settings) {
         if (typeof Settings == "string") {
-            Settings = { Token: Settings }
+            Settings = { Token: Settings, LogLevel: 6, Intents: 3276799 }
         }
 
-        this.Token = Settings.Token
+        this.Token = `Bot ${Settings.Token}`
 
         this.API = new API(this.Token, this)
-        this.Gateway = new Gateway(this.Token, this)
+        this.Gateway = new Gateway(this.Token, Settings.Intents, this)
         this.Logger = new Logger("Duacord", Settings.LogLevel)
+    }
+
+    async Login() {
+        this.Logger.Info("Connecting to the discord gateway...")
+        await this.Gateway.Connect()
+        this.Logger.Info("Connected to the discord gateway!")
     }
 }
 
